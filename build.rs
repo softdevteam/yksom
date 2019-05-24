d0285c988c77449739c9684e14ad8542cfe3837b
Laurence Tratt <laurie@tratt.net>
2019-05-24 12:18:42 +0200

    Use rerun_except to cut down on unnecessary builds.
    Changes to lang_tests/*.som caused a full rebuild each time, which was
    increasingly irritating.

--- build.rs
+++ build.rs
@@ -14,13 +14,17 @@ extern crate lrpar;
 use cfgrammar::yacc::YaccKind;
 use lrlex::LexerBuilder;
 use lrpar::CTParserBuilder;
+use rerun_except::rerun_except;
 
 fn main() -> Result<(), Box<std::error::Error>> {
+    rerun_except(&["/lang_tests/*.som"])?;
+
     let lex_rule_ids_map = CTParserBuilder::new()
         .yacckind(YaccKind::Grmtools)
         .process_file_in_src("lib/compiler/som.y")?;
     LexerBuilder::new()
         .rule_ids_map(lex_rule_ids_map)
         .process_file_in_src("lib/compiler/som.l")?;
+
     Ok(())
 }
