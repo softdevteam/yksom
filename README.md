85a86f66f4601268f11fb28e8b90177f017c8f4d
Laurence Tratt <laurie@tratt.net>
2019-09-20 14:11:54 +0100

    Flesh out the README a little bit.
    Since this links to the internal API docs, it also makes sense to flesh those
    out a little bit too.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # ykSOM
 
-A Yorick SOM interpreter.
+A [SOM](http://som-st.github.io/) VM in Rust. SOM is a cut-down Smalltalk-like
+language. yksom is eventually intended to be used with
+[Yorick](https://github.com/softdevteam/yk/) to produce a JIT-compiling VM,
+though it is currently an entirely stand-alone interpreter. Currently it is
+partly a test bed to experiment with good ways of structuring Rust
+interpreters, balancing correctness, performance, and readability. The
+[internal API documentation](https://softdevteam.github.io/yksom/api/) is
+available online.
+
+yksom is intended to be source-compatible with other SOM implementations
+although it implements (uncheckable) exceptions, so that (one day...) errors
+can cause SOM-level backtraces.
--- src/lib/vm/mod.rs
+++ src/lib/vm/mod.rs
@@ -7,6 +7,16 @@
 // at your option. This file may not be copied, modified, or distributed except according to those
 // terms.
 
+//! The yksom run-time. The run-time uses trait objects but stores them as thin pointers (using the
+//! experimental [natrob library](https://github.com/softdevteam/natrob). This allows integer
+//! tagging to be used. The [`Val`](vm::val::Val) struct stores tagged integers / pointers to boxed
+//! objects. The [`Obj`](vm::objects::Obj) trait is the "supertype" trait of all objects, but it is
+//! not intended that most parts of the VM call [`Obj`](vm::objects::Obj) directly: one should use
+//! the identically named methods on [`Val`](vm::val::Val). As this suggests, users should, in most
+//! cases, either operate directly on a [`Val`](vm::val::Val) or
+//! [`Val::downcast`](vm::val::Val::downcast) (or
+//! [`Val::try_downcast`](vm::val::Val::try_downcast)) it to a concrete implementation of `Obj`.
+
 /// If a `ValResult` represents an error, percolate it up the call stack.
 #[macro_export]
 macro_rules! vtry {
