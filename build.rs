9aaedf8b4cbf8e8ec8b127d16cdd3c81a802d1bf
Laurence Tratt <laurie@tratt.net>
2020-03-09 16:41:40 +0000

    Changes to the SOM library shouldn't recompile the Rust interpreter.

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use lrpar::CTParserBuilder;
 use rerun_except::rerun_except;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    rerun_except(&["/lang_tests/*.som"])?;
+    rerun_except(&["/lang_tests/*.som", "lib/SOM"])?;
 
     let lex_rule_ids_map = CTParserBuilder::new()
         .yacckind(YaccKind::Grmtools)
