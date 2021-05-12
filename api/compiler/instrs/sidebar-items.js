d4607f3f41ea881dc01c6704e401f23d07f159d7
github_hooks <noreply@soft-dev.org>
2021-05-12 18:01:13 +0100

    Deploy gh-pages

---
+++ api/compiler/instrs/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Instr",""],["Primitive",""]],"struct":[["UpVarDef",""]]});
---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["yksom"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"yksom/vm/objects/struct.ThinObj.html\" title=\"struct yksom::vm::objects::ThinObj\">ThinObj</a>","synthetic":false,"types":["yksom::vm::objects::ThinObj"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
